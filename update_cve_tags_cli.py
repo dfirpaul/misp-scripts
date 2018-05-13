0eb1c2f06061108b33645499529dea7ccf2f5346
siniysv1 <ivan.s.sim@gmail.com>
2018-05-13 23:48:38 +0400

    Cleanup

--- update_cve_tags_cli.py
+++ update_cve_tags_cli.py
@@ -11,14 +11,11 @@ import argparse
 from tqdm import tqdm
 import sys
 
-import warnings
-warnings.filterwarnings("ignore")
-
 cve_pattern = re.compile('CVE-\d{4}-\d{4}')
+
 
 def load_file(filename, ip_column, cve_column, cve_pattern, delimiter=','):
     output = {}
-
     try:
         with open(filename, 'r') as csv_file:
             cve_uniq = []
@@ -31,7 +28,6 @@ def load_file(filename, ip_column, cve_column, cve_pattern, delimiter=','):
                 except Exception as e_parse_row:
                     logging.info('Cannot parse row, exception: {}'.format(str(e_parse_row)))
             cve_uniq = list(set(cve_uniq))
-
             for cve in cve_uniq:
                 output[cve] = []
                 csv_file.seek(0)
@@ -41,27 +37,11 @@ def load_file(filename, ip_column, cve_column, cve_pattern, delimiter=','):
                             output[cve].append(row[ip_column])
                     except Exception as e_parse_row:
                         logging.info('Cannot parse row, exception: {}'.format(str(e_parse_row)))
-
     except Exception as e_openfile:
         logging.error('Error while opening the file: {}'.format(str(e_openfile)))
         return False
-
     return output
 
-
-
-def update_attribute_sighting(data, misp):
-    for cve, ips in data.items():
-        for ip in ips:
-            doc = {}
-            doc["values"] = [cve]
-            doc["source"] = ip
-            doc["type"] = 0
-            doc["timestamp"] = str(time.time())
-            print ("Input JSON to MISP: {}".format(doc))
-            res = misp.set_sightings(doc)
-            print ("MISP response: {}".format(res))
-    return True
 
 def update_event_tags(data, misp):
     # cve_ids = {}
@@ -77,7 +57,6 @@ def update_event_tags(data, misp):
             attribute_uuids[attribute['value']] = attribute['uuid']
     # print(event_ids)
     # print(attribute_uuids)
-
     for event_id in event_ids:
         event = misp.get(event_id)
         attributes = event['Event']['Attribute']
@@ -88,7 +67,6 @@ def update_event_tags(data, misp):
                         if 'is_vulnerable' in tag['name']:
                             # print('Tag:', tag)
                             misp.untag(attribute['uuid'], tag['name'])
-
     updated_attributes_count = 0
     for cve, uuid in tqdm(attribute_uuids.items()):
         # print(cve, uuid)
@@ -98,8 +76,8 @@ def update_event_tags(data, misp):
         response = misp.tag(uuid, tag)
         # print(response)
         updated_attributes_count += 1
-
     return 'Updated attributes: {}'.format(str(updated_attributes_count))
+
 
 def init(url, key, verifycert):
     try:
@@ -110,13 +88,12 @@ def init(url, key, verifycert):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Assing tags with number of internal vulnerable hosts to attributes.')
-    parser.add_argument("-f", "--file", required=False, default='vuln_data.csv', help="CSV file with information about vulnerabilities.")
+    parser.add_argument("-f", "--file", required=True, help="CSV file with information about vulnerabilities.")
     parser.add_argument("-i", "--ipcolumn", required=False, type=int, default=1, help="Index of a column with IP address.")
     parser.add_argument("-c", "--cvecolumn", required=False, type=int, default=4, help="Index of a column with CVE.")
     parser.add_argument("-d", "--delimiter", required=False, type=str, default=',', help="CSV delimiter.")
     parser.add_argument("-u", "--url", required=True, type=str, help="MISP URL.")
     parser.add_argument("-k", "--key", required=True, type=str, help="MISP key.")
-    parser.add_argument("-ssl", "--verify_cert", required=False, default=True, help="Verify MISP certificate?")
     parser.add_argument('--verify-cert', dest='misp_verifycert', action='store_true')
     parser.add_argument('--no-verify-cert', dest='misp_verifycert', action='store_false')
     parser.set_defaults(misp_verifycert=True)
@@ -125,8 +102,10 @@ if __name__ == '__main__':
     misp_url = args.url
     misp_key = args.key
     misp_verifycert = args.misp_verifycert
+    if not misp_verifycert:
+        import warnings
+        warnings.filterwarnings("ignore")
     misp = init(misp_url, misp_key, misp_verifycert)
-
     if isinstance(misp, str):
         print('Cannot init MISP connection: {}'.format(misp))
         sys.exit(1)
@@ -135,6 +114,9 @@ if __name__ == '__main__':
     ip_column = args.ipcolumn
     cve_column = args.cvecolumn
     delimiter = args.delimiter
+
     data = load_file(filename, ip_column, cve_column, cve_pattern, delimiter)
 
     result = update_event_tags(data, misp)
+
+    sys.exit(0)
